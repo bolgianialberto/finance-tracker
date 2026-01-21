239fa3b07a23c666dae4bc10fb8dbfd446bacd5b
Alberto Bolgiani <albertobolgiani9@gmail.com>
2026-01-21 09:04:43 +0100

    fintrac first commit

--- hooks/use-theme-color.ts
+++ hooks/use-theme-color.ts
@@ -1,21 +1,15 @@
-/**
- * Learn more about light and dark modes:
- * https://docs.expo.dev/guides/color-schemes/
- */
-
 import { Colors } from "@/constants/colors";
 import { useColorScheme } from "@/hooks/use-color-scheme";
 
+type ThemeProps = {
+  light?: string;
+  dark?: string;
+};
+
 export function useThemeColor(
-  props: { light?: string; dark?: string },
+  props: ThemeProps,
   colorName: keyof typeof Colors.light & keyof typeof Colors.dark,
 ) {
   const theme = useColorScheme() ?? "light";
-  const colorFromProps = props[theme];
-
-  if (colorFromProps) {
-    return colorFromProps;
-  } else {
-    return Colors[theme][colorName];
-  }
+  return props[theme] ?? Colors[theme][colorName];
 }
